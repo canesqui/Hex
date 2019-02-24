a78f08ddf3fb98d6f49c7fc633ef2fd0dab80b57
johnboggess <johnboggess@southern.edu>
2019-02-24 18:44:43 -0500

    added missing files

---
+++ Server/AI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class AI : Player
+    {
+        public Common.Move MakeMove(GameLogicPlaceholder game)
+        {
+            return Think();
+        }
+
+        public Common.Move Think()
+        {
+            Common.Move move = new Common.Move();
+            return move;
+        }
+    }
+}
---
+++ Server/GameLogicPlaceholder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class GameLogicPlaceholder
+    {
+        Random rng = new Random();
+        bool isPlayer1sTurn = true;
+
+        public Common.MoveResponse Move(Common.Move move)
+        {
+            int i = rng.Next(0, 4);
+
+            if (i == 0)
+            {
+                SwitchTurns();
+                return Common.MoveResponse.Sucess;
+            }
+            else if(i==1)
+            {
+                return Common.MoveResponse.NotYourTurn;
+            }
+            else if(i==2)
+            {
+                return Common.MoveResponse.IllegalMove;
+            }
+            return Common.MoveResponse.GameOver;
+        }
+
+        public void SwitchTurns()
+        {
+            isPlayer1sTurn = !isPlayer1sTurn;
+        }
+
+        public bool IsPlayer1sTurn()
+        {
+            return isPlayer1sTurn;
+        }
+    }
+}
