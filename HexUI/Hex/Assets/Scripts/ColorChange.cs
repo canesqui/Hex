5508f59ea03f097ec41e8b816ee8e71449b980ed
Rajiv Injety <rajivinjety@Rajivs-MacBook-Pro.local>
2019-04-07 19:10:08 -0400

    HexUI

---
+++ HexUI/Hex/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	public Color[]colors; // allows input of material colors in a set sized araay
+	public SpriteRenderer rend;  // what are we rendering? the hex
+
+	public enum Player {ONE, TWO};
+	public Player currentPlayer = Player.ONE;
+
+	//private int index = 1; //initialize at 1, otherwise you have to press the ball twice to change color
+
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer> (); // gives functionality for the renderer
+	}
+
+	void NextPlayer() {
+
+       if( currentPlayer == Player.ONE ) {
+          currentPlayer = Player.TWO;
+       }
+       else if( currentPlayer == Player.TWO) {
+          currentPlayer = Player.ONE;
+       }
+    }
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+	// if there are no colors present nothing happens
+		if (colors.Length == 0){
+		return;
+		}
+	
+	//		index += 1; // when mouse is pressed down we incrememnt up to the next index location
+			// when it reaches the end of the colors it stars over
+	//		if (index == colors.Length +1){
+	//			index = 1;
+	//		}
+	//		print (index); // used for debugging
+	//		rend.color = colors [index - 1]; // this sets the material color values inside the index
+
+		if (currentPlayer == Player.ONE)
+			rend.color = colors[0];
+		else if (currentPlayer == Player.TWO)
+			rend.color = colors[1];
+			
+			NextPlayer();
+	}
+
+
+
+
+
+}
---
+++ HexUI/Hex/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadNextScene ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		Debug.Log ("Next Scene Loaded");
+	}
+
+	public void LoadScene (string name)
+	{
+		SceneManager.LoadScene(name);
+		Debug.Log ("Scene Loaded: " + name);
+	}
+
+	public void Quit ()
+	{
+		Debug.Log ("Quit Request");
+		Application.Quit();
+		Debug.Log ("Quit Successful");
+	}
+
+}
