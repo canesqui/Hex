d26061ce002fe2aa104a6aa121683d96784e6ed1
Alec <alecennis@southern.edu>
2019-04-17 20:34:19 -0400

    test change.

---
+++ AI/AI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    public class AI
+    {
+            public void Dijkstra()
+            {
+                
+            }
+
+            public void Heuristic()
+            {
+
+            }
+
+            public void MiniMax()
+            {
+
+            }
+
+            public void Other()
+            {
+
+            }
+    }
+}
---
+++ TEST_AI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AI;
+
+namespace TEST_AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new AI.AI();
+            test.
+        }
+    }
+}
