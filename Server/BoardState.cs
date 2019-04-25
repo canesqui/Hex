8289a6dfbc67641c2589044b38e61ff9cb0c20a5
Sarah <sarahwu@southern.edu>
2019-04-25 15:30:03 -0400

    Add File to track and update board state

---
+++ Server/BoardState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class BoardState
+    {
+        public int[,] board = new int[11, 11];
+
+        public void InitializeBoard()
+        {
+            Array.Clear(board, 0, 121);
+        }
+
+        public void UpdateBoard(Common.Move move, bool player1)
+        {
+            if (player1)
+            {
+                board[move.x, move.y] = 1;
+            }
+            else
+            {
+                board[move.x, move.y] = 2;
+            }
+
+        }
+
+     }
+}
